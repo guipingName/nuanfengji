a0f4cb44b7c96a1a5a0fd1daddb026de7c8d8ca1
guipingName <guipingme@sina.com>
2017-06-22 11:01:18 +0800

    123

---
+++ warmwind/Application/gping.h
@@ -0,0 +1,13 @@
+//
+//  gping.h
+//  warmwind
+//
+//  Created by guiping on 2017/6/22.
+//  Copyright © 2017年 galaxyWind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface gping : UIView
+
+@end
---
+++ warmwind/Application/gping.m
@@ -0,0 +1,21 @@
+//
+//  gping.m
+//  warmwind
+//
+//  Created by guiping on 2017/6/22.
+//  Copyright © 2017年 galaxyWind. All rights reserved.
+//
+
+#import "gping.h"
+
+@implementation gping
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
